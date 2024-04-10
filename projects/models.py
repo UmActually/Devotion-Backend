9b0ba49b27aaae3fc9449ebc152699ddbda9e40a
UmActually <89704397+UmActually@users.noreply.github.com>
2024-04-09 19:40:49 -0600

    Accurate dummy data automation for end-to-end tests

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,8 @@ class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64, null=False, blank=False)
     description = models.TextField(max_length=1024, null=True, blank=True)
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, related_name="children", null=True, blank=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="projects")
     leaders = models.ManyToManyField("users.User", related_name="leader_of")
     members = models.ManyToManyField("users.User", related_name="member_of")
 
--- tasks/models.py
+++ tasks/models.py
@@ -17,15 +17,18 @@ class TaskPriority(models.IntegerChoices):
 
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=64, null=False, blank=False)
+    name = models.CharField(max_length=128, null=False, blank=False)
     description = models.TextField(max_length=1024, null=True, blank=True)
     status = models.SmallIntegerField(choices=TaskStatus.choices, null=False, blank=False)
     priority = models.SmallIntegerField(choices=TaskPriority.choices, null=False, blank=False)
     start_date = models.DateField(null=False, blank=False)
     due_date = models.DateField(null=False, blank=False)
-    parent_project = models.ForeignKey("projects.Project", on_delete=models.CASCADE, null=False, blank=False)
-    parent_task = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
-    asignee = models.ForeignKey("users.User", on_delete=models.CASCADE, null=False, blank=False)
+    parent_project = models.ForeignKey(
+        "projects.Project", on_delete=models.CASCADE, null=False, blank=False, related_name="tasks")
+    parent_task = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="tasks")
+    asignee = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, null=False, blank=False, related_name="tasks")
 
     def __str__(self):
         return self.name
